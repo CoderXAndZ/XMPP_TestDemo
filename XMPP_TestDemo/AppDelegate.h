04b4967d8d5bf00bf92bbf2eb74f51be5203512b
ios_domain <1935722630@qq.com>
2018-06-20 10:19:51 +0800

    Initial Commit

---
+++ XMPP_TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPP_TestDemo
+//
+//  Created by admin on 2018/6/20.
+//  Copyright © 2018年 XZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
